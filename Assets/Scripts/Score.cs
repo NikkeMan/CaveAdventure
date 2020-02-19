f94218d6ca1c3d9ef9e853ec4e90eecb5c0a96d5
Niko Leppänoro <nikoleppanoro@outlook.com>
2020-02-19 18:38:56 +0200

    Score counter for HUD
    - added a simple score counter that uses TextMeshPro
    - added "Retro Gaming" font (made by Daymarius at dafont.com)

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] int currentScore;
+    // public Text scoreText;
+    public TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnScoreChanged();
+    }
+
+    public void AddToScore(int scoreAmount) {
+        currentScore += scoreAmount;
+        OnScoreChanged();
+    }
+
+    void OnScoreChanged() {
+        //scoreText.text = "Score: " + currentScore.ToString("D9");
+        scoreText.text = currentScore.ToString("D9");
+    }
+}
