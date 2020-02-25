e3450c1d74af7529f45b310def6255937279a698
Niko Leppänoro <nikoleppanoro@outlook.com>
2020-02-25 14:29:56 +0200

    Lava collision with player
    - added a script to lava tilemap so that lava deals damage to player
    - lava tilemap collision box needs to be lowered so that lava tiles don't hit the player when they're overlapping with ground tiles
    - forgot to add Enemy tags to Bat and its gameobjects

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Lava : MonoBehaviour
+{
+    [SerializeField] float hurtBoxTimer = 1.0f;
+    [SerializeField] float hurtBoxTimerMax = 1.0f;
+    HealthBar healthBar;
+    int damageAmount = 1;
+    [SerializeField] TilemapCollider2D tileMapCollider;
+
+    void Start()
+    {
+        healthBar = GameObject.Find("HealthPanel").GetComponent<HealthBar>();
+        tileMapCollider = gameObject.GetComponent<TilemapCollider2D>();
+    }
+
+    private void FixedUpdate() {
+        if (hurtBoxTimer < hurtBoxTimerMax) {
+            hurtBoxTimer += Time.deltaTime;
+        }
+        if (hurtBoxTimer >= hurtBoxTimerMax && tileMapCollider.isActiveAndEnabled == false) {
+            tileMapCollider.enabled = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            healthBar.TakeDamage(damageAmount);
+            tileMapCollider.enabled = false;
+            hurtBoxTimer = 0;
+        }
+    }
+}
