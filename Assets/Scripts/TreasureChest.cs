f857fd8545068ad9225d6b4922e80739cd01c976
Niko Leppänoro <nikoleppanoro@outlook.com>
2020-02-26 13:03:32 +0200

    Treasure Chests
    - added treasure chests
    - renamed some prefabs

---
+++ Assets/Scripts/TreasureChest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour
+{
+    [SerializeField] int scoreAmount = 500;
+    [SerializeField] bool hasKey = false;
+    Score score;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GameObject.Find("ScorePanel").GetComponent<Score>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            score.AddToScore(scoreAmount);
+            animator.SetBool("isOpen", true);
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+
+            if (hasKey) {
+                //add a key to inventory:
+                //Debug.Log("You got a key. Nice!");
+            }
+        }
+    }
+}
