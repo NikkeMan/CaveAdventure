63c0abf74e97f2ecd6f114e509a1202adbadc9ae
JeKoski <46312455+JeKoski@users.noreply.github.com>
2020-02-27 19:46:53 +0200

    Intro level base
    Also transitions, scene switcher etc.

---
+++ Assets/Scripts/ENDLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENDLevel : MonoBehaviour
+{
+    [SerializeField] SceneSwitch sceneSwitcher;
+
+    private void Start()
+    {
+        sceneSwitcher = GameObject.Find("SceneLoader").GetComponent<SceneSwitch>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            sceneSwitcher.LoadScene();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [Header("References:")]
+    [SerializeField] Animator transitionAnimator;
+
+    [Header("Choose method:")]
+    [SerializeField] private bool autoLoadNextScene = true;
+    [SerializeField] private bool loadSceneByName = false;
+
+    [Header("If not auto-loading:")]
+    [SerializeField] private string sceneName;
+    [SerializeField] private int levelToLoad = 0;
+
+    [Header("Wait transition for:")]
+    [SerializeField] private float transitionTime = 1f;
+
+    public void LoadScene()
+    {
+        StartCoroutine(LoadSceneCoRoutine());
+    }
+
+    IEnumerator LoadSceneCoRoutine()
+    {
+        transitionAnimator.SetTrigger("TransitionStart");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        if (autoLoadNextScene)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        else if (loadSceneByName && !autoLoadNextScene)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        else if (!loadSceneByName && !autoLoadNextScene)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+        else
+        {
+            Debug.Log("No Scene Load method selected!");
+        }
+    }
+}
+
+
+
+
